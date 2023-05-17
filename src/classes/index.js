98f8a70fab877d3a4edccc71b017de20409b3a00
Yohana <yovis.1423@gmail.com>
2023-05-17 17:38:44 -0500

    Primer commit

---
+++ src/classes/index.js
@@ -0,0 +1,9 @@
+/// Es para poder centraliozar todas las importaciones de la clases
+
+import { Todo } from "./todo.class";
+import { TodoList } from "./todo-list.class";
+
+export {
+    Todo,
+    TodoList
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
-
-import { saludar } from "./js/componentes";
 import  './style.css';
 
-const nombre = 'Yohana';
+import { Todo, TodoList } from "./classes";
+import { crearTodoHTML } from './js/componentes';
 
-saludar(nombre);
+
+export const todoList = new TodoList();
+
+todoList.todos.forEach( todo => crearTodoHTML (todo));
+
+// const newTodo = new Todo('Aprender JavaScript');
+// // todoList.nuevoTodo(newTodo);
+
+// todoList.todos[0].imprimirClase();
+// // newTodo.imprimirClase();
+
+console.log('todos', todoList.todos);
